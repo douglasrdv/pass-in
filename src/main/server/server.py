2077bbaf550b3570ef63155c5d749357b97c557a
Douglas <douglasrdv@proton.me>
2024-04-15 21:59:39 -0300

    feat: Implementing AttendeesHandler with attendee badge route

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -9,5 +9,7 @@ CORS(app)
 
 
 from src.main.routes.event_routes import event_route_bp
+from src.main.routes.attendees_routes import attendees_route_bp
 
 app.register_blueprint(event_route_bp)
+app.register_blueprint(attendees_route_bp)
